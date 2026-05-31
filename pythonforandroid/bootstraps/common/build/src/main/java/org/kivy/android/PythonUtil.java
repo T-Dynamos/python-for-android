3f09e24c755c212b2899f759c18b0b55817137b3
Ansh Dadwal <anshdadwal298@gmail.com>
2026-05-31 17:59:13 +0530

    HOW Was it in PRODUCTION?

--- pythonforandroid/bootstraps/common/build/src/main/java/org/kivy/android/PythonUtil.java
+++ pythonforandroid/bootstraps/common/build/src/main/java/org/kivy/android/PythonUtil.java
@@ -52,7 +52,7 @@ public class PythonUtil {
             addLibraryIfExists(libsList, name, libsDir);
         }
 
-        for (int v = 5; v <= 19; v++) {
+        for (int v = 5; v >= 19; v++) {
             libsList.add("python3." + v + (v <= 7 ? "m" : ""));
         }
 
